99884976b63d451514cf5af65ba42f643f853021
Eric Angle <angle@angle.txcorp.com>
2013-08-16 12:45:24 -0600

    bisection working now

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 double iterate(double g(double x, double eps, double y), double root, double eps, double y, double tol, int& n);
-double bisect();
+double bisect(double g(double x, double eps, double y), double left, double right, double eps, double y, double tol, int& n);
 double secant();
 
 double g_fpi(double x, double eps, double y);
@@ -46,65 +46,52 @@ int main() {
       y = Y[j];
 
       cout << "eps = " << eps << ", y = " << y << endl;
+      cout << "--------------------" << endl;      
 
       // Fixed point iteration
       n = 0;
       root = y;
       root = iterate(g_fpi, root, eps, y, tol, n);
       if (n == 1000000) {
-        cout << "Did not converge." << endl;
+        cout << "FPI: Did not converge." << endl;
       }
       else {
-        cout << root << '\t' << n << endl; 
+        cout << "FPI: " << root << '\t' << n << endl; 
       }
 
       // Newton
       n = 0;
-      root = iterate(g_fpi, root, eps, y, tol, n);
+      root = y;
+      root = iterate(g_newton, root, eps, y, tol, n);
       if (n == 1000000) {
-        cout << "Did not converge." << endl;
+        cout << "NEW: Did not converge." << endl;
       }
       else {
-        cout << root << '\t' << n << endl;
+        cout << "NEW: " << root << '\t' << n << endl;
       }
 
       // Interval bisection
       n = 0;
-      root = iterate(g_fpi, y, eps, y, tol, n);
+      double left = 0.0, right = 3.14159265358979323846;
+      root = bisect(f, left, right, eps, y, tol, n);
       if (n == 1000000) {
-        cout << "Did not converge." << endl;
+        cout << "BIS: Did not converge." << endl;
       }
       else {
-        cout << root << '\t' << n << endl;
+        cout << "BIS: " << root << '\t' << n << endl;
       }
-
-/*
-                        ! BISECTION method
-                        ! ------------------------------------------------------
-                        lint = 0.d0                                     ! initial left bound
-                        rint = 3.1415926535897932d0     ! initial right bound
-                        n = 0
-                        CALL bisect(lint, rint, root, bis, tol, eps, manom, n, dec)
-                        ! bissol(i,j) = root
-                        WRITE (*,8) root
-8                       format (1PG22.14)
-                        IF (n.GT.1000000) THEN
-                                PRINT *,'Did not converge.'
-                        ELSE
-                                PRINT *,n
-                        END IF
-*/
 
       // Secant
       n = 0;
+      root = y;
       root = iterate(g_fpi, y, eps, y, tol, n);
       if (n == 1000000) {
-        cout << "Did not converge." << endl;
+        cout << "SEC: Did not converge." << endl;
       }
       else {
-        cout << root << '\t' << n << endl;
+        cout << "SEC: " << root << '\t' << n << endl;
       }
-
+      cout << endl;
     }    
   }
 
@@ -123,74 +110,60 @@ double iterate(double g(double x, double eps, double y), double root, double eps
   return root;
 }
 
+// SUBROUTINE iteratesecant(oldroot, root, f, tol, eps, manom, n, dec)
+//                implicit none
+//                double precision oldroot, old1, root, f, tol, eps, manom, new1, new2
+//                integer n, dec
+//102             old1 = oldroot
+//                oldroot = root
+//                root = f(old1, oldroot, eps, manom)
+//                IF (dec.EQ.0) THEN
+//                        new1 = f(oldroot, root, eps, manom)
+//                        new2 = f(root, new1, eps, manom)
+//                        CALL aitken(root, new1, new2, tol)
+//                END IF
+//                n = n + 1                                               ! counts number of iterations
+//                IF (n.GT.1000000) GOTO 202                      ! stops if not converging
+//                IF (dabs(root-oldroot).GE.tol) GOTO 102
+//202     RETURN
+//        END
 
-        ! SECANT subroutine
-        ! ------------------------------------------------------------
-        SUBROUTINE iteratesecant(oldroot, root, f, tol, eps, manom, n, dec)
-                implicit none
-                double precision oldroot, old1, root, f, tol, eps, manom, new1, new2
-                integer n, dec
-102             old1 = oldroot
-                oldroot = root
-                root = f(old1, oldroot, eps, manom)
-                IF (dec.EQ.0) THEN
-                        new1 = f(oldroot, root, eps, manom)
-                        new2 = f(root, new1, eps, manom)
-                        CALL aitken(root, new1, new2, tol)
-                END IF
-                n = n + 1                                               ! counts number of iterations
-                IF (n.GT.1000000) GOTO 202                      ! stops if not converging
-                IF (dabs(root-oldroot).GE.tol) GOTO 102
-202     RETURN
-        END
+double bisect(double g(double x, double eps, double y), double left, double right, double eps, double y, double tol, int& n) {
+  double error = tol + 1.0;
+  double root;
 
+  double fleft, fright, middle, fmiddle;
 
-double bisect() {
-  return 0;
+  while (error > tol && n < 1000000) {
+    fleft = g(left, eps, y);      // initial left bound
+    fright = g(right, eps, y);    // initial right bound
+
+    middle = (left + right)/2.0;      // bisect interval
+    fmiddle = g(middle, eps, y);
+
+    if (fleft*fmiddle <= 0.0) {   // if root to left of middle
+      right = middle;              // right bound becomes middle
+    }
+    else {                                              
+      left = middle;               // left bound becomes middle
+    }
+    root = (left + right)/2.0;
+    error = abs(left - right);
+    n++;
+  }
+  return root;
 }
-
-/*
-        ! BISECTION subroutine
-        ! ------------------------------------------------------------
-        SUBROUTINE bisect(left, right, middle, f, tol, eps, manom, n, dec)
-                implicit none
-                double precision left, right, middle, f, fleft, fright, fmiddle, tol, eps, manom
-                double precision root, old
-                integer n, dec
-
-                fleft = f(left, eps, manom)             ! initial left bound
-                fright = f(right, eps, manom)           ! initial right bound
-
-101             middle = (left+right)/2.d0                      ! bisect interval
-                fmiddle = f(middle, eps, manom)
-
-                IF (fleft*fmiddle.LE.0.d0) THEN ! if root to left of middle
-                        right = middle                          ! right bound becomes middle
-                ELSE                                                            ! otherwise,
-                        left = middle                                   ! left bound becomes middle
-                        fleft = fmiddle
-                END IF
-
-                old = middle
-                root = (left+right)/2.d0
-
-                n = n + 1                                               ! counts number of iterations
-                IF (n.GT.1000000) GOTO 201                      ! stops if not converging
-                IF (dabs(left-right).GE.tol) GOTO 101
-201     RETURN
-        END
-*/
 
 double secant() {
   return 0;
 }
 
 double g_fpi(double x, double eps, double y) {
-  return y + eps*sin(x);
+  return x - f(x, eps, y);
 }
 
 double g_newton(double x, double eps, double y) {
-  return x + (y - x + eps*sin(x))/(1.0 - eps*cos(x));
+  return x - f(x, eps, y)/fPrime(x, eps, y);
 }
 
 double f(double x, double eps, double y) {
@@ -205,13 +178,12 @@ double g_secant(double x_n, double x_nmm, double eps, double y) {
   return x_n - (x_n - x_nmm)*f(x_n,eps,y)/(f(x_n,eps,y) - f(x_nmm,eps,y));
 }
 
-/* Aitken's acceleration
-SUBROUTINE aitken(root, new1, new2, tol)
-  implicit none
-  double precision root, new1, new2, numer, denom, tol
-  numer = root*new2 - new1*new1
-  denom = new2 - 2.d0*new1 + root
-  IF (denom.GT.tol) THEN
-    root = numer/denom
-  END IF
-*/
+// Aitken's acceleration
+//SUBROUTINE aitken(root, new1, new2, tol)
+//  implicit none
+//  double precision root, new1, new2, numer, denom, tol
+//  numer = root*new2 - new1*new1
+//  denom = new2 - 2.d0*new1 + root
+//  IF (denom.GT.tol) THEN
+//    root = numer/denom
+//  END IF
