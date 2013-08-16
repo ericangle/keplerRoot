5a95897633ad77206f056f3ee7e557e3c12217e1
Eric Angle <angle@angle.txcorp.com>
2013-08-16 13:26:06 -0600

    secant working

--- main.cpp
+++ main.cpp
@@ -13,9 +13,10 @@
 #include <cmath>
 using namespace std;
 
-double iterate(double g(double x, double eps, double y), double root, double eps, double y, double tol, int& n);
-double bisect(double g(double x, double eps, double y), double left, double right, double eps, double y, double tol, int& n);
-double secant();
+// Can probably combine iterate and iterate secant
+double iterate(double g(double x, double eps, double y), double root, double eps, double y, double tol, int& n, int stop);
+double iterateSecant(double g(double x_n, double x_nmm, double eps, double y), double root, double oldRoot, double eps, double y, double tol, int& n, int stop);
+double bisect(double f(double x, double eps, double y), double left, double right, double eps, double y, double tol, int& n, int stop);
 
 double g_fpi(double x, double eps, double y);
 double g_newton(double x, double eps, double y);
@@ -24,73 +25,49 @@ double g_secant(double x, double z, double eps, double y);
 double f(double x, double eps, double y);
 double fPrime(double x, double eps, double y);
 
+void outputLine(string algType, double root, int n, int stop);
+
 int main() {
+  double pi = 3.14159265358979323846;
+  double piOverTwo = pi/2.0;  
+
   // Tolerance: want roots accurate to 14 decimal places
   double tol = 0.5e-14;
 
   // Eccentricity values
   double E [] = {0.0, 0.1, 0.2, 0.4, 0.8, 0.9, 0.95, 0.975, 0.9875};
-
-  // Mean anomaly values: 0.0, 0.1, ..., 3.1
-  double Y [32];
-  for (int i = 0; i < 32; i++) {
-    Y[i] = ((double) i)/10.0;
-  }
  
   double eps, y, root;
   int n;
+  int stop = 1000000; // stop after this many steps
 
   for (int i = 0; i < 9; i++) {
     eps = E[i];
     for (int j = 0; j < 32; j++) {
-      y = Y[j];
-
+      y = ((double) j)/10.0; // Mean anomaly values: 0.0, 0.1, ..., 3.1
       cout << "eps = " << eps << ", y = " << y << endl;
       cout << "--------------------" << endl;      
 
       // Fixed point iteration
       n = 0;
-      root = y;
-      root = iterate(g_fpi, root, eps, y, tol, n);
-      if (n == 1000000) {
-        cout << "FPI: Did not converge." << endl;
-      }
-      else {
-        cout << "FPI: " << root << '\t' << n << endl; 
-      }
+      root = iterate(g_fpi, piOverTwo, eps, y, tol, n, stop);
+      outputLine("FPI", root, n, stop);
 
       // Newton
       n = 0;
-      root = y;
-      root = iterate(g_newton, root, eps, y, tol, n);
-      if (n == 1000000) {
-        cout << "NEW: Did not converge." << endl;
-      }
-      else {
-        cout << "NEW: " << root << '\t' << n << endl;
-      }
+      root = iterate(g_newton, piOverTwo, eps, y, tol, n, stop);
+      outputLine("NEW", root, n, stop);
 
       // Interval bisection
       n = 0;
-      double left = 0.0, right = 3.14159265358979323846;
-      root = bisect(f, left, right, eps, y, tol, n);
-      if (n == 1000000) {
-        cout << "BIS: Did not converge." << endl;
-      }
-      else {
-        cout << "BIS: " << root << '\t' << n << endl;
-      }
+      root = bisect(f, 0.0, pi, eps, y, tol, n, stop);
+      outputLine("BIS", root, n, stop);
 
       // Secant
       n = 0;
-      root = y;
-      root = iterate(g_fpi, y, eps, y, tol, n);
-      if (n == 1000000) {
-        cout << "SEC: Did not converge." << endl;
-      }
-      else {
-        cout << "SEC: " << root << '\t' << n << endl;
-      }
+      root = iterateSecant(g_secant, piOverTwo, 0.0, eps, y, tol, n, stop);
+      outputLine("SEC", root, n, stop);      
+
       cout << endl;
     }    
   }
@@ -98,10 +75,10 @@ int main() {
   return 0;
 }
   
-double iterate(double g(double x, double eps, double y), double root, double eps, double y, double tol, int& n) {
+double iterate(double g(double x, double eps, double y), double root, double eps, double y, double tol, int& n, int stop) {
   double error = tol + 1.0;
   double old;
-  while (error > tol && n < 1000000) {
+  while (error > tol && n < stop) {
     old = root;
     root = g(old, eps, y);
     error = abs(root - old);
@@ -110,36 +87,31 @@ double iterate(double g(double x, double eps, double y), double root, double eps
   return root;
 }
 
-// SUBROUTINE iteratesecant(oldroot, root, f, tol, eps, manom, n, dec)
-//                implicit none
-//                double precision oldroot, old1, root, f, tol, eps, manom, new1, new2
-//                integer n, dec
-//102             old1 = oldroot
-//                oldroot = root
-//                root = f(old1, oldroot, eps, manom)
-//                IF (dec.EQ.0) THEN
-//                        new1 = f(oldroot, root, eps, manom)
-//                        new2 = f(root, new1, eps, manom)
-//                        CALL aitken(root, new1, new2, tol)
-//                END IF
-//                n = n + 1                                               ! counts number of iterations
-//                IF (n.GT.1000000) GOTO 202                      ! stops if not converging
-//                IF (dabs(root-oldroot).GE.tol) GOTO 102
-//202     RETURN
-//        END
+double iterateSecant(double g(double x_n, double x_nmm, double eps, double y), double root, double oldRoot, double eps, double y, double tol, int& n, int stop) {
+  double error = tol + 1.0;
+  double old, older;
+  while (error > tol && n < stop) {
+    old = root;
+    older = oldRoot;
+    root = g(old, older, eps, y);
+    oldRoot = old;
+    error = abs(root - old);
+    n++;
+  }
+  return root;
+}
 
-double bisect(double g(double x, double eps, double y), double left, double right, double eps, double y, double tol, int& n) {
+double bisect(double f(double x, double eps, double y), double left, double right, double eps, double y, double tol, int& n, int stop) {
   double error = tol + 1.0;
   double root;
-
   double fleft, fright, middle, fmiddle;
 
-  while (error > tol && n < 1000000) {
-    fleft = g(left, eps, y);      // initial left bound
-    fright = g(right, eps, y);    // initial right bound
+  while (error > tol && n < stop) {
+    fleft = f(left, eps, y);      // initial left bound
+    fright = f(right, eps, y);    // initial right bound
 
     middle = (left + right)/2.0;      // bisect interval
-    fmiddle = g(middle, eps, y);
+    fmiddle = f(middle, eps, y);
 
     if (fleft*fmiddle <= 0.0) {   // if root to left of middle
       right = middle;              // right bound becomes middle
@@ -154,16 +126,16 @@ double bisect(double g(double x, double eps, double y), double left, double righ
   return root;
 }
 
-double secant() {
-  return 0;
-}
-
 double g_fpi(double x, double eps, double y) {
   return x - f(x, eps, y);
 }
 
 double g_newton(double x, double eps, double y) {
   return x - f(x, eps, y)/fPrime(x, eps, y);
+}
+
+double g_secant(double x_n, double x_nmm, double eps, double y) {
+  return x_n - (x_n - x_nmm)*f(x_n,eps,y)/(f(x_n,eps,y) - f(x_nmm,eps,y));
 }
 
 double f(double x, double eps, double y) {
@@ -174,8 +146,14 @@ double fPrime(double x, double eps, double y) {
   return 1.0 - eps*cos(x);
 }
 
-double g_secant(double x_n, double x_nmm, double eps, double y) {
-  return x_n - (x_n - x_nmm)*f(x_n,eps,y)/(f(x_n,eps,y) - f(x_nmm,eps,y));
+void outputLine(string algType, double root, int n, int stop) {
+  if (n == stop) {
+    cout << algType << ": Did not converge.";
+  }
+  else {
+    cout << algType << ": " << root << '\t' << n;
+  }
+  cout << endl;
 }
 
 // Aitken's acceleration
